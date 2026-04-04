cc154e54d2f5da6b5505bef9d5d3f7ed612b6b38
mat-ha3 <67873769+mat-ha3@users.noreply.github.com>
2026-04-04 20:00:31 +0100

    Camera Movement, Start Scene & Player Creator

--- Assets/Board/Scripts/Player.cs
+++ Assets/Board/Scripts/Player.cs
@@ -4,7 +4,18 @@ public class Player : MonoBehaviour
 {
     
     // VARIABLES
+    private string name;
+    private string character;
     
+    // CONSTRUCTOR
+    public Player(string name, string character)
+    {
+        this.name = name;
+        this.character = character;
+        
+        Debug.Log("Player Created: Name = " + name + " Character = " + character + "");
+    }
+
     // the tile the player is assigned to (the parent gameobject)
     private BoardSpace position;
 
@@ -14,7 +25,11 @@ public class Player : MonoBehaviour
         this.position = tile;
     }
     
-    // BASIC GETTERS
+    // GETTERS
+    public string getName()
+    {
+        return name;
+    }
     public BoardSpace getPosition()
     {
         return position;
